a07c6ad930dc9534933dcc0f1263202840190dc7
Akira Serikawa <akserikawa@gmail.com>
2022-05-04 21:46:33 +0200

    Setting up the base framework for the blockchain

--- main.go
+++ main.go
@@ -1,5 +1,65 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+)
 
+type BlockChain struct {
+	blocks []*Block
+}
+
+type Block struct {
+	Hash     []byte
+	Data     []byte
+	PrevHash []byte
+}
+
+func (b *Block) DeriveHash() {
+	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+	hash := sha256.Sum256(info)
+	b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+	block := &Block{
+		Hash:     []byte{},
+		Data:     []byte(data),
+		PrevHash: prevHash,
+	}
+	block.DeriveHash()
+	return block
+}
+
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{
+		[]*Block{
+			Genesis(),
+		},
+	}
+}
+
+func main() {
+	chain := InitBlockChain()
+
+	chain.AddBlock("1st Block after Genesis")
+	chain.AddBlock("2nd Block after Genesis")
+	chain.AddBlock("3rd Block after Genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 }
