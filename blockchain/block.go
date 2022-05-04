a997bb626ed42650e85f33087a211f61de407198
Akira Serikawa <akserikawa@gmail.com>
2022-05-04 23:00:51 +0200

    Add Proof of Work algorithm

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,10 +1,5 @@
 package blockchain
 
-import (
-	"bytes"
-	"crypto/sha256"
-)
-
 type BlockChain struct {
 	Blocks []*Block
 }
@@ -13,12 +8,7 @@ type Block struct {
 	Hash     []byte
 	Data     []byte
 	PrevHash []byte
-}
-
-func (b *Block) DeriveHash() {
-	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
-	hash := sha256.Sum256(info)
-	b.Hash = hash[:]
+	Nonce    int
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
@@ -26,8 +16,13 @@ func CreateBlock(data string, prevHash []byte) *Block {
 		Hash:     []byte{},
 		Data:     []byte(data),
 		PrevHash: prevHash,
+		Nonce:    0,
 	}
-	block.DeriveHash()
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
